619cd5eae50324edc6fcc93fb8293ed7c30af4cf
zhengmeiyu <zhengmeiyu@baidu.com>
2017-05-02 20:22:46 +0800

    20170502

--- index.js
+++ index.js
@@ -2,20 +2,46 @@
  * test case
  * by zhengmeiyu@baidu.com
  */
-var testObj = Object.create();
+var testObj = Object.create(null);
 // test URL
 testObj.url = 'https://m.baidu.com';
 
 var page = require('webpage').create();
+var system = require('system');
+
+// third party library: console colors
+var colors = require('colors');
+
+colors.setTheme({
+  silly: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red'
+});
+
+// 开始时间
+var startTime = Date.now();
 
 // first screen capture + load time
 page.open(testObj.url, function(status) {
-  console.log("first screen start");
+  console.log('\nFirst screen start:'.debug);
 
-  if(status === "success") {
+  if(status === 'success') {
+    console.log('Loading time ' + (Date.now() - startTime) + ' msec');
     page.render('screenCapture/baidu-first_screen_capture.png');
+    console.log('[Success] '.info + ' to generate first screen capture.');
+  } else {
+  	console.log('[FAIL] '.error + ' to load the address!');
   }
 
-  console.log("first screen end");
+  console.log('First screen end!\n'.debug);
+
+
   phantom.exit();
 });
